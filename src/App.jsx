0e2e9f3332bbddc45140e0e1949383e9b567ab7f
sheikhmhummadhasan <hasan2001onlin@gmail.com>
2026-03-20 00:08:40 +0600

    Secend commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import './Comp-css/Header.css'
+import Header from './Components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <div className="nav-box-1">
+                        <img src="src/assets/header-logo.png" alt="Logo" />
+                        <h2>HasaN Portfolio</h2>
+                    </div>
+                    <div className="nav-box-2"></div>
+                    <div className="nav-box-3"></div>
+                </nav>
+            </header>
+        </div>
+    );
+};
+
+export default Header;
